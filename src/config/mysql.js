2a95fc68bde13278cd2669f7ccb8323727b2dce8
bagus tri harjanto <bagustri15@gmail.com>
2021-07-09 17:42:55 +0700

    update some code test

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,8 +1,14 @@
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  // host: 'localhost',
+  // user: 'root',
+  // password: '',
+  // database: 'pos_app',
+  // timezone: 'UTC'
+  host: '54.205.184.83',
+  port: 3306,
+  user: 'golden',
+  password: 'password',
   database: 'pos_app',
   timezone: 'UTC'
 })
